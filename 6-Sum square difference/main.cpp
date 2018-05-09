3bcbb5af33c30c6d3f053364f7e567a32e843692
islammohsen <islammohsen2@hotmail.com>
2018-05-10 00:38:50 +0200

    added solution for problem 6

---
+++ 6-Sum square difference/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n; //in problem n = 100
+    int sum1 = (n * (n + 1)) / 2;
+    sum1 *= sum1;
+    int sum2 = ((n) * (n + 1) * (2 * n + 1)) / 6;
+    cout << sum1 - sum2;
+    return 0;
+}
